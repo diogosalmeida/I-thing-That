1acb0c983305cc36d387539805cc88daf90229bc
diogosalmeida <diogo.almeida@mercadolivre.com>
2023-11-14 10:16:32 -0300

    add config

---
+++ src/main/java/com/ithinkthat/IThingThatApplication.java
@@ -0,0 +1,13 @@
+package com.ithinkthat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IThingThatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IThingThatApplication.class, args);
+	}
+
+}
